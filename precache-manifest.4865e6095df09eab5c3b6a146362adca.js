82cbaf4aa43cf0755cf1d18bd56a087255bd5e89
a-elnawawy <eng.ahmed.elnawawy@gmail.com>
2020-01-21 21:18:25 +0200

    Updates

---
+++ precache-manifest.4865e6095df09eab5c3b6a146362adca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96524c300665702ce9239629f9452b49",
+    "url": "/robo-friends-react-course/index.html"
+  },
+  {
+    "revision": "44bb1a889bb62483b03a",
+    "url": "/robo-friends-react-course/static/css/main.23f3b4f5.chunk.css"
+  },
+  {
+    "revision": "20e30690ee891c56a2e4",
+    "url": "/robo-friends-react-course/static/js/2.1c2ebb62.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robo-friends-react-course/static/js/2.1c2ebb62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44bb1a889bb62483b03a",
+    "url": "/robo-friends-react-course/static/js/main.332be352.chunk.js"
+  },
+  {
+    "revision": "1eb30f53846688c6b881",
+    "url": "/robo-friends-react-course/static/js/runtime-main.664fdfc7.js"
+  }
+]);
