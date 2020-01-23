89cd0ddcc77b9fce87f1cb77b6068dfa2f43337f
a-elnawawy <eng.ahmed.elnawawy@gmail.com>
2020-01-23 14:46:40 +0200

    reorder components and containers & add redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import App from './App';
+import App from './containers/App';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
